d4920bc5ac3ea8d0b029165fe83ed95d3245c09e
Brian <bbleck@unm.edu>
2020-01-15 12:31:13 -0500

    initial commit

---
+++ changer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import subprocess
+
+interface = input("interface > ")
+newMac = input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + newMac)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + newMac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
